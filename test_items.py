b168feba75a87ac3fc898bbc028cfcb17be8d752
VladimirZvezdkin <102977532+VladimirZvezdkin@users.noreply.github.com>
2022-04-20 16:30:43 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_basket_button(browser):
     browser.get(link)
     # time.sleep(30) #please uncomment this string for checking
     basket = browser.find_element(By.CLASS_NAME, value='btn-add-to-basket')
